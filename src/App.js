aead40514dc7d882a1912ec51170cd2cdcb0cda6
Jonny Rivas <jonrimo@gmail.com>
2021-10-03 12:22:58 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -5,18 +5,31 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <main>
+              <div className="row card-letter">
+                  <div className="col-sm-8">
+                      <div className="card">
+                          <div className="card-body">
+                              <h5 className="card-title">Notice title</h5>
+                              <p className="card-text">Content</p>
+                              <a href="#">URL</a>
+                              <p>Author</p>
+                              <p>date public</p>
+                          </div>
+                      </div>
+                  </div>
+                  <div className="col-sm-4">
+                      <div className="card">
+                          <div className="card-body">
+                              <h5 className="card-title">Weather "Description"</h5>
+                              <p className="card-text">observation time</p>
+                              <p>temperature</p>
+                              <p></p>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </main>
       </header>
     </div>
   );
